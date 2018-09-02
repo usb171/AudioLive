568da666eab1c6a5e0bb466e836af7f18031c9ee
Nilton <usb171@gmail.com>
2018-09-02 11:49:24 -0300

    Em operação

--- sala/models.py
+++ sala/models.py
@@ -12,7 +12,7 @@ class Sala(models.Model):
     nome = models.CharField('Nome da Página', max_length=20, null=True, unique=True)
     # pagina = models.OneToOneField(Group, on_delete=models.CASCADE, null=True)
     usuarios = models.ManyToManyField(User, blank=True)
-    stream = models.CharField('Tipo da Comunicação', max_length=20, choices=_STREAM, default="None", blank=True, null=True)
+    # stream = models.CharField('Tipo da Comunicação', max_length=20, choices=_STREAM, default="None", blank=True, null=True)
 
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     update_at = models.DateTimeField('Atualizado em', auto_now_add=True)
